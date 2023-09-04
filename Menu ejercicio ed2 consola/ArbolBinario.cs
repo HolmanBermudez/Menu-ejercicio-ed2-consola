4c75dec7dd5ec8baff8e9978c29137fcfe8ce229
Admin <Admin@DESKTOP-VBJGGPH>
2023-09-04 09:52:13 +0200

    Agregar archivos de proyecto.

---
+++ Menu ejercicio ed2 consola/ArbolBinario.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_ejercicio_ed2_consola
+{
+    internal class ArbolBinario
+    {
+        public Nodo Raiz;
+
+        public ArbolBinario()
+        {
+            Raiz = null;
+        }
+
+        public void Insertar( Nodo raiz, string entrada)
+        {
+            Insertarreg( raiz, entrada);
+
+        }
+        public Nodo Insertarreg( Nodo raiz, string entrada)
+        {
+            if (raiz == null)
+            {
+                raiz = new Nodo(entrada);
+                return raiz;
+            }
+            if (string.Compare(entrada, raiz.Entrada) < 0)
+            {
+                Raiz.left=Insertarreg( raiz.left, entrada);
+            }
+            else if (string.Compare(entrada, raiz.Entrada) > 0)
+            {
+                Raiz.right=Insertarreg( raiz.right, entrada);
+            }
+            return raiz;
+
+        }
+        public Nodo Buscar(Nodo inicio, string dato)
+        {
+            if (inicio == null)
+            {
+                return inicio;
+            }
+            if (string.Compare(dato, inicio.entrada) > 0)
+            {
+                return Buscar(inicio.right, dato);
+            }
+            if (string.Compare(dato, inicio.entrada) < 0)
+            {
+                return Buscar(inicio.left, dato);
+            }
+            return inicio;
+        }
+        public void PrintInOrder(Nodo raiz)
+        {
+            if (raiz == null)
+            {
+                PrintInOrder(raiz.left);
+                Console.WriteLine($" entrada:{raiz.Entrada}");
+                PrintInOrder(raiz.right);
+            }
+        }
+        public void PrintPreOrden(Nodo raiz)
+        {
+            if (raiz == null)
+            {
+                Console.WriteLine($"entrada:{raiz.Entrada}");
+                PrintPreOrden(raiz.left);
+                PrintPreOrden(raiz.right);
+            }
+        }
+        public void PrintPosOrden(Nodo raiz)
+        {
+            if (raiz == null)
+            {
+                PrintPreOrden(raiz.left);
+                PrintPreOrden(raiz.right);
+                Console.WriteLine($"entrada:{raiz.Entrada}");
+
+            }
+        }
+    }
+}
---
+++ Menu ejercicio ed2 consola/Nodo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_ejercicio_ed2_consola
+{
+    internal class Nodo
+    {
+
+        public string entrada;
+        public Nodo right;
+        public Nodo left;
+
+        public string Entrada { get => entrada; set => entrada = value; }
+        public Nodo Right { get => right; set => right = value; }
+        public Nodo Left { get => left; set => left = value; }
+
+        public Nodo(string entrada)
+        {
+            this.Entrada = entrada;
+            this.Right = null;
+            this.Left = null;
+        }
+    }
+}
